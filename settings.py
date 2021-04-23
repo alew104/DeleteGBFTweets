f75832cc1821c83da40b6991c2a1dd7fcda6a1eb
Alex Lew <alew104@gmail.com>
2021-04-22 20:57:05 -0700

    oops i committed my keys

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 #Put your application keys and access tokens here
 
-consumer_key = "oh7YrITg4qMPqAuNDjScnkLLO"
-consumer_secret = "K8ItRcYeiGehtKerNj1aerNAmuQbZ81y7F81i1HxXARQTG1yBM"
-access_token = "3146662231-T0qTdtAFmHfOtbHHrzIgwV1n2PzbmnaZCdNK2zp"
-access_token_secret = "vHvGLdWPjJ8onykLrCho9BnKafLFFknFN6OByFPEUscJ2"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 delete_hashtag = "#GranblueFantasy!"
 regex = "ID:\s\w{8}"
 
